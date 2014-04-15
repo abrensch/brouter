a440505f6fcbba8a48c85af21e1421ebedac677c
unknown <Arndt.Brenschede@web.de>
2014-04-15 10:31:40 +0200

    added new param threadsize

--- misc/scripts/standalone/server.cmd
+++ misc/scripts/standalone/server.cmd
@@ -6,4 +6,4 @@ REM java -cp brouter.jar btools.brouter.RouteServer <segmentdir> <profile-map> <
 set JAVA_OPTS=-Xmx128M -Xms128M -Xmn8M
 set CLASSPATH=../brouter.jar
 
-java %JAVA_OPTS% -cp %CLASSPATH% btools.server.RouteServer ..\segments2 ..\profiles2 17777
+java %JAVA_OPTS% -cp %CLASSPATH% btools.server.RouteServer ..\segments2 ..\profiles2 17777 1
--- misc/scripts/standalone/server.sh
+++ misc/scripts/standalone/server.sh
@@ -6,4 +6,4 @@
 JAVA_OPTS="-Xmx128M -Xms128M -Xmn8M"
 CLASSPATH=../brouter.jar
 
-java $JAVA_OPTS -cp $CLASSPATH btools.server.RouteServer ../segments2 ../profiles2 17777
+java $JAVA_OPTS -cp $CLASSPATH btools.server.RouteServer ../segments2 ../profiles2 17777 1
