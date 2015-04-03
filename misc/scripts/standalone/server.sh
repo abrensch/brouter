f3d2500f3ac45f9f504c4fc2bc02ff7036686a76
Norbert Renner <norbert.renner@gmx.de>
2015-04-03 11:44:49 +0200

    set maxRunningTime=300 as default timeout in standalone scripts

--- misc/scripts/standalone/server.sh
+++ misc/scripts/standalone/server.sh
@@ -3,7 +3,8 @@
 # BRouter standalone server
 # java -cp brouter.jar btools.brouter.RouteServer <segmentdir> <profile-map> <customprofiledir> <port> <maxthreads>
 
-JAVA_OPTS="-Xmx128M -Xms128M -Xmn8M"
+# maxRunningTime is the request timeout in seconds, set to 0 to disable timeout
+JAVA_OPTS="-Xmx128M -Xms128M -Xmn8M -DmaxRunningTime=300"
 CLASSPATH=../brouter.jar
 
 java $JAVA_OPTS -cp $CLASSPATH btools.server.RouteServer ../segments3 ../profiles2 ../customprofiles 17777 1
