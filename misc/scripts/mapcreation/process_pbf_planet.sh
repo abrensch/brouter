3a9c74a910cfe3853796817279610f1f0f241e46
Arndt <Arndt.Brenschede@web.de>
2015-11-01 11:58:30 +0100

    1.3.2 preparations

--- misc/scripts/mapcreation/process_pbf_planet.sh
+++ misc/scripts/mapcreation/process_pbf_planet.sh
@@ -9,7 +9,7 @@ fi
 
 touch lastmaprun.date
 
-rm -rf /var/www/brouter/segments3_lastrun
+rm -rf /var/www/brouter/segments4_lastrun
 
 mkdir tmp
 cd tmp
@@ -34,15 +34,26 @@ mkdir unodes55
 /java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -Ddeletetmpfiles=true -DuseDenseMaps=true btools.mapcreator.PosUnifier nodes55 unodes55 bordernids.dat bordernodes.dat /private-backup/srtm
 
 mkdir segments
-mkdir segments/carsubset
 
-/java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -DuseDenseMaps=true btools.mapcreator.WayLinker unodes55 waytiles55 bordernodes.dat ../lookups.dat ../car-test.brf segments/carsubset cd5
-/java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -Ddeletetmpfiles=true -DuseDenseMaps=true btools.mapcreator.WayLinker unodes55 waytiles55 bordernodes.dat ../lookups.dat ../all.brf segments rd5
+/java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -DuseDenseMaps=true btools.mapcreator.WayLinker unodes55 waytiles55 bordernodes.dat ../lookups.dat ../all.brf segments rd5
+
+mkdir traffic
+
+/java/bin/java -Xmx2600m -Xms2600m -Xmn32m -jar ../brouter.jar segments 8.593025 49.724868 seed 0 ../car-traffic_analysis.brf
+
+/java/bin/java -Xmx2600m -Xms2600m -Xmn32m -jar ../brouter.jar segments 8.609011 50.527861 seed 0 ../car-traffic_analysis.brf
+
+/java/bin/java -Xmx2600m -Xms2600m -Xmn32m -jar ../brouter.jar segments 12.867994 51.239889 seed 0 ../car-traffic_analysis.brf
+
+/java/bin/java -Xmx2600m -Xms2600m -Xmn32m -jar ../brouter.jar segments 11.128099 49.501845 seed 0 ../car-traffic_analysis.brf
+
+
+/java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -DuseDenseMaps=true btools.mapcreator.WayLinker unodes55 waytiles55 bordernodes.dat ../lookups.dat ../all.brf segments rd5
 
 cd ..
 rm -rf segments
 mv tmp/segments segments
-rm -rf tmp
-cp /var/www/brouter/segments3/.htaccess segments
-mv /var/www/brouter/segments3 /var/www/brouter/segments3_lastrun
-mv segments /var/www/brouter/segments3
+cp /var/www/brouter/segments4/.htaccess segments
+cp /var/www/brouter/segments4/storageconfig.txt segments
+mv /var/www/brouter/segments4 /var/www/brouter/segments4_lastrun
+mv segments /var/www/brouter/segments4
