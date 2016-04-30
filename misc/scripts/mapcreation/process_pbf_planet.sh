99147635cd35a182e60d533b46d047df2bb75b47
Arndt <Arndt.Brenschede@web.de>
2016-04-30 17:01:19 +0200

    changes memsizes, added easter-europe to traffic-sim

--- misc/scripts/mapcreation/process_pbf_planet.sh
+++ misc/scripts/mapcreation/process_pbf_planet.sh
@@ -14,10 +14,10 @@ rm -rf /var/www/brouter/segments4_lastrun
 mkdir tmp
 cd tmp
 mkdir nodetiles
-/java/bin/java -Xmx256m -Xms256m -Xmn32m -cp ../pbfparser.jar:../brouter.jar btools.mapcreator.OsmCutter ../lookups.dat nodetiles ways.dat relations.dat ../all.brf ../planet-latest.osm.pbf
+/java/bin/java -Xmx2600m -Xms2600m -Xmn32m -cp ../pbfparser.jar:../brouter.jar btools.mapcreator.OsmCutter ../lookups.dat nodetiles ways.dat relations.dat ../all.brf ../planet-latest.osm.pbf
 
 mkdir ftiles
-/java/bin/java -Xmx512M -Xms512M -Xmn32M -cp ../brouter.jar -Ddeletetmpfiles=true -DuseDenseMaps=true btools.mapcreator.NodeFilter nodetiles ways.dat ftiles
+/java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -Ddeletetmpfiles=true -DuseDenseMaps=true btools.mapcreator.NodeFilter nodetiles ways.dat ftiles
 
 /java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -Ddeletetmpfiles=true -DuseDenseMaps=true btools.mapcreator.RelationMerger ways.dat ways2.dat relations.dat ../lookups.dat ../trekking.brf ../softaccess.brf
 
@@ -28,7 +28,7 @@ mkdir waytiles55
 /java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -Ddeletetmpfiles=true -DuseDenseMaps=true btools.mapcreator.WayCutter5 ftiles waytiles waytiles55 bordernids.dat
 
 mkdir nodes55
-/java/bin/java -Xmx128M -Xms128M -Xmn32M -cp ../brouter.jar -Ddeletetmpfiles=true -DuseDenseMaps=true btools.mapcreator.NodeCutter ftiles nodes55
+/java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -Ddeletetmpfiles=true -DuseDenseMaps=true btools.mapcreator.NodeCutter ftiles nodes55
 
 mkdir unodes55
 /java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -Ddeletetmpfiles=true -DuseDenseMaps=true btools.mapcreator.PosUnifier nodes55 unodes55 bordernids.dat bordernodes.dat /private-backup/srtm
@@ -47,6 +47,9 @@ mkdir traffic
 
 /java/bin/java -Xmx2600m -Xms2600m -Xmn32m -jar ../brouter.jar segments 11.128099 49.501845 seed 0 ../car-traffic_analysis.brf
 
+/java/bin/java -Xmx2600m -Xms2600m -Xmn32m -jar ../brouter.jar segments 16.532815 49.169541 seed 0 ../car-traffic_analysis.brf
+
+/java/bin/java -Xmx2600m -Xms2600m -Xmn32m -jar ../brouter.jar segments 16.917636 51.040949 seed 0 ../car-traffic_analysis.brf
 
 /java/bin/java -Xmx2600M -Xms2600M -Xmn32M -cp ../brouter.jar -DuseDenseMaps=true btools.mapcreator.WayLinker unodes55 waytiles55 bordernodes.dat ../lookups.dat ../all.brf segments rd5
 
@@ -57,3 +60,4 @@ cp /var/www/brouter/segments4/.htaccess segments
 cp /var/www/brouter/segments4/storageconfig.txt segments
 mv /var/www/brouter/segments4 /var/www/brouter/segments4_lastrun
 mv segments /var/www/brouter/segments4
+rm -rf tmp
