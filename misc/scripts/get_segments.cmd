a0790d5dcaca99e2792dba6290267202db6ce930
maciejczaja <57909287+maciejczaja@users.noreply.github.com>
2021-01-29 12:27:45 +0100

    Improve and move get_statements.cmd script

---
+++ misc/scripts/get_segments.cmd
@@ -0,0 +1,19 @@
+@echo off
+
+set SEGMENTS=E10_N45.rd5 E15_N45.rd5 E15_N50.rd5 E20_N45.rd5 E20_N50.rd5
+
+IF EXIST "..\segments4" (
+    set SEGMENTSPATH=..\segments4
+    goto download
+)
+
+IF EXIST "..\..\segments4" (
+    set SEGMENTSPATH=..\..\segments4
+    goto download
+)
+
+:download
+
+(for %%s in (%SEGMENTS%) do (
+   curl.exe --output %SEGMENTSPATH%/%%s --url brouter.de/brouter/segments4/%%s
+))
