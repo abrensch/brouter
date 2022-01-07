31e7c4ebbd278803e8cf47fe0fa03bbbad490b25
Manuel Fuhr <manuel@knid.net>
2022-01-07 08:20:47 +0100

    Move info and button to own views

--- brouter-routing-app/src/main/java/btools/routingapp/BInstallerView.java
+++ brouter-routing-app/src/main/java/btools/routingapp/BInstallerView.java
@@ -15,12 +15,9 @@ import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.View;
 
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
-
-import btools.router.RoutingHelper;
 
 public class BInstallerView extends View {
   public static final int MASK_SELECTED_RD5 = 1;
@@ -31,34 +28,17 @@ public class BInstallerView extends View {
   private final Bitmap bmp;
   private final float[] testVector = new float[2];
   private final int[] tileStatus;
-  private final File segmentDir;
   private final Matrix mat;
-  private final Matrix matText;
   private final GestureDetector mGestureDetector;
   private final ScaleGestureDetector mScaleGestureDetector;
-  Paint pnt_1 = new Paint();
-  Paint pnt_2 = new Paint();
-  Paint paint = new Paint();
-  int btnh = 40;
-  int btnw = 160;
-  private int imgwOrig;
-  private int imghOrig;
-  private float scaleOrig;
-  private int imgw;
-  private int imgh;
+  Paint paintGrid = new Paint();
+  Paint paintTiles = new Paint();
   private float viewscale;
   private boolean tilesVisible = false;
-  private long availableSize;
-  private long totalSize = 0;
-  private long rd5Tiles = 0;
-  private long delTiles = 0;
-  private OnClickListener mOnClickListener;
+  private OnSelectListener mOnSelectListener;
 
   public BInstallerView(Context context, AttributeSet attrs) {
     super(context, attrs);
-
-    File baseDir = ConfigHelper.getBaseDir(getContext());
-    segmentDir = new File(baseDir, "brouter/segments4");
 
     try {
       AssetManager assetManager = getContext().getAssets();
@@ -70,10 +50,13 @@ public class BInstallerView extends View {
     }
 
     tileStatus = new int[72 * 36];
-    matText = new Matrix();
     mat = new Matrix();
     mGestureDetector = new GestureDetector(context, new GestureListener());
     mScaleGestureDetector = new ScaleGestureDetector(context, new ScaleGestureListener());
+  }
+
+  public void setOnSelectListener(OnSelectListener listener) {
+    mOnSelectListener = listener;
   }
 
   private void setRatio(float ratio, float focusX, float focusY) {
@@ -89,12 +72,20 @@ public class BInstallerView extends View {
     setRatio(ratio, focusX, focusY);
   }
 
-  public void setAvailableSize(long availableSize) {
-    this.availableSize = availableSize;
-  }
-
   public void setTileStatus(int tileIndex, int tileMask) {
     tileStatus[tileIndex] |= tileMask;
+    if (mOnSelectListener != null) {
+      mOnSelectListener.onSelect();
+    }
+    invalidate();
+  }
+
+  public void toggleTileStatus(int tileIndex, int tileMask) {
+    tileStatus[tileIndex] ^= tileMask;
+    if (mOnSelectListener != null) {
+      mOnSelectListener.onSelect();
+    }
+    invalidate();
   }
 
   public void clearAllTilesStatus(int tileMask) {
@@ -103,6 +94,9 @@ public class BInstallerView extends View {
         int tileIndex = gridPos2Tileindex(ix, iy);
         tileStatus[tileIndex] ^= tileStatus[tileIndex] & tileMask;
       }
+    }
+    if (mOnSelectListener != null) {
+      mOnSelectListener.onSelect();
     }
     invalidate();
   }
@@ -121,11 +115,6 @@ public class BInstallerView extends View {
     return selectedTiles;
   }
 
-  @Override
-  public void setOnClickListener(OnClickListener listener) {
-    mOnClickListener = listener;
-  }
-
   private int gridPos2Tileindex(int ix, int iy) {
     return (35 - iy) * 72 + (ix >= 70 ? ix - 70 : ix + 2);
   }
@@ -139,30 +128,22 @@ public class BInstallerView extends View {
 
   // get back the current image scale
   private float currentScale() {
-    testVector[1] = 1.f;
+    testVector[0] = 1.f;
     mat.mapVectors(testVector);
-    return testVector[1] / viewscale;
+    return testVector[0] / viewscale;
   }
 
   @Override
   protected void onSizeChanged(int w, int h, int oldw, int oldh) {
     super.onSizeChanged(w, h, oldw, oldh);
 
-    imgwOrig = getWidth();
-    imghOrig = getHeight();
-    int im = Math.max(imgwOrig, imghOrig);
-
-    scaleOrig = im / 480.f;
-
-    matText.preScale(scaleOrig, scaleOrig);
-
-    imgw = (int) (imgwOrig / scaleOrig);
-    imgh = (int) (imghOrig / scaleOrig);
+    int imgwOrig = getWidth();
+    int imghOrig = getHeight();
 
     float scaleX = imgwOrig / ((float) bmp.getWidth());
     float scaleY = imghOrig / ((float) bmp.getHeight());
 
-    viewscale = Math.min(scaleX, scaleY);
+    viewscale = Math.max(scaleX, scaleY);
 
     mat.postScale(viewscale, viewscale);
     tilesVisible = false;
@@ -180,93 +161,47 @@ public class BInstallerView extends View {
     float fh = ih / 36.f;
 
     if (tilesVisible) {
-      pnt_1.setColor(Color.GREEN);
-      pnt_1.setStyle(Paint.Style.STROKE);
+      paintGrid.setColor(Color.GREEN);
+      paintGrid.setStyle(Paint.Style.STROKE);
       for (int ix = 0; ix < 72; ix++) {
         for (int iy = 0; iy < 36; iy++) {
           int tidx = gridPos2Tileindex(ix, iy);
           int tilesize = BInstallerSizes.getRd5Size(tidx);
           if (tilesize > 0) {
-            canvas.drawRect(fw * ix, fh * (iy + 1), fw * (ix + 1), fh * iy, pnt_1);
+            canvas.drawRect(fw * ix, fh * (iy + 1), fw * (ix + 1), fh * iy, paintGrid);
           }
         }
       }
-    }
 
-    rd5Tiles = 0;
-    delTiles = 0;
-    totalSize = 0;
-    int mask2 = MASK_SELECTED_RD5 | MASK_DELETED_RD5 | MASK_INSTALLED_RD5;
-    int mask3 = mask2 | MASK_CURRENT_RD5;
+      int mask2 = MASK_SELECTED_RD5 | MASK_DELETED_RD5 | MASK_INSTALLED_RD5;
+      int mask3 = mask2 | MASK_CURRENT_RD5;
 
-    pnt_2.setStyle(Paint.Style.STROKE);
-    pnt_2.setColor(Color.GRAY);
-    pnt_2.setStrokeWidth(1);
-    drawSelectedTiles(canvas, pnt_2, fw, fh, MASK_INSTALLED_RD5, mask3, false, false, tilesVisible);
-    pnt_2.setColor(Color.BLUE);
-    pnt_2.setStrokeWidth(1);
-    drawSelectedTiles(canvas, pnt_2, fw, fh, MASK_INSTALLED_RD5 | MASK_CURRENT_RD5, mask3, false, false, tilesVisible);
-    pnt_2.setColor(Color.GREEN);
-    pnt_2.setStrokeWidth(2);
-    drawSelectedTiles(canvas, pnt_2, fw, fh, MASK_SELECTED_RD5, mask2, true, false, tilesVisible);
-    pnt_2.setColor(Color.YELLOW);
-    pnt_2.setStrokeWidth(2);
-    drawSelectedTiles(canvas, pnt_2, fw, fh, MASK_SELECTED_RD5 | MASK_INSTALLED_RD5, mask2, true, false, tilesVisible);
-    pnt_2.setColor(Color.RED);
-    pnt_2.setStrokeWidth(2);
-    drawSelectedTiles(canvas, pnt_2, fw, fh, MASK_DELETED_RD5 | MASK_INSTALLED_RD5, mask2, false, true, tilesVisible);
-
-    canvas.setMatrix(matText);
-
-    paint.setColor(Color.RED);
-
-    long mb = 1024 * 1024;
-
-    if (!this.tilesVisible) {
-      paint.setTextSize(35);
-      canvas.drawText("Touch region to zoom in!", 30, (imgh / 3) * 2, paint);
-    }
-    paint.setTextSize(20);
-
-
-    String totmb = ((totalSize + mb - 1) / mb) + " MB";
-    String freemb = availableSize >= 0 ? ((availableSize + mb - 1) / mb) + " MB" : "?";
-    canvas.drawText("Selected segments=" + rd5Tiles, 10, 25, paint);
-    canvas.drawText("Size=" + totmb + " Free=" + freemb, 10, 45, paint);
-
-
-    String btnText = null;
-    if (delTiles > 0) btnText = "Delete " + delTiles + " tiles";
-    else if (rd5Tiles > 0) btnText = "Start Download";
-    else if (this.tilesVisible &&
-      rd5Tiles == 0 &&
-      RoutingHelper.hasDirectoryAnyDatafiles(segmentDir)) btnText = "Update all";
-
-    if (btnText != null) {
-      paint.setStyle(Paint.Style.STROKE);
-      canvas.drawRect(imgw - btnw, imgh - btnh, imgw - 2, imgh - 2, paint);
-      paint.setStyle(Paint.Style.FILL);
-      canvas.drawText(btnText, imgw - btnw + 5, imgh - 10, paint);
+      paintTiles.setStyle(Paint.Style.STROKE);
+      paintTiles.setColor(Color.GRAY);
+      paintTiles.setStrokeWidth(1);
+      drawSelectedTiles(canvas, paintTiles, fw, fh, MASK_INSTALLED_RD5, mask3);
+      paintTiles.setColor(Color.BLUE);
+      paintTiles.setStrokeWidth(1);
+      drawSelectedTiles(canvas, paintTiles, fw, fh, MASK_INSTALLED_RD5 | MASK_CURRENT_RD5, mask3);
+      paintTiles.setColor(Color.GREEN);
+      paintTiles.setStrokeWidth(2);
+      drawSelectedTiles(canvas, paintTiles, fw, fh, MASK_SELECTED_RD5, mask2);
+      paintTiles.setColor(Color.YELLOW);
+      paintTiles.setStrokeWidth(2);
+      drawSelectedTiles(canvas, paintTiles, fw, fh, MASK_SELECTED_RD5 | MASK_INSTALLED_RD5, mask2);
+      paintTiles.setColor(Color.RED);
+      paintTiles.setStrokeWidth(2);
+      drawSelectedTiles(canvas, paintTiles, fw, fh, MASK_DELETED_RD5 | MASK_INSTALLED_RD5, mask2);
     }
   }
 
-  private void drawSelectedTiles(Canvas canvas, Paint pnt, float fw, float fh, int status, int mask, boolean doCount, boolean cntDel, boolean doDraw) {
+  private void drawSelectedTiles(Canvas canvas, Paint pnt, float fw, float fh, int status, int mask) {
     for (int ix = 0; ix < 72; ix++)
       for (int iy = 0; iy < 36; iy++) {
         int tidx = gridPos2Tileindex(ix, iy);
         if ((tileStatus[tidx] & mask) == status) {
           int tilesize = BInstallerSizes.getRd5Size(tidx);
           if (tilesize > 0) {
-            if (doCount) {
-              rd5Tiles++;
-              totalSize += BInstallerSizes.getRd5Size(tidx);
-            }
-            if (cntDel) {
-              delTiles++;
-              totalSize += BInstallerSizes.getRd5Size(tidx);
-            }
-            if (!doDraw)
-              continue;
             // draw cross
             canvas.drawLine(fw * ix, fh * iy, fw * (ix + 1), fh * (iy + 1), pnt);
             canvas.drawLine(fw * ix, fh * (iy + 1), fw * (ix + 1), fh * iy, pnt);
@@ -310,36 +245,31 @@ public class BInstallerView extends View {
     return retVal || super.onTouchEvent(event);
   }
 
+  interface OnSelectListener {
+    void onSelect();
+  }
+
   class GestureListener extends GestureDetector.SimpleOnGestureListener {
 
     @Override
     public boolean onSingleTapConfirmed(MotionEvent e) {
-      // Download Button
-      if ((delTiles > 0 || rd5Tiles >= 0) && e.getX() > imgwOrig - btnw * scaleOrig && e.getY() > imghOrig - btnh * scaleOrig) {
-        if (mOnClickListener != null) {
-          mOnClickListener.onClick(null);
-        }
-        invalidate();
-        return true;
-      }
-
       if (tilesVisible) {
         Matrix imat = new Matrix();
         if (mat.invert(imat)) {
-          float[] touchpoint = {e.getX(), e.getY()};
-          imat.mapPoints(touchpoint);
+          float[] touchPoint = {e.getX(), e.getY()};
+          imat.mapPoints(touchPoint);
 
-          int tidx = tileIndex(touchpoint[0], touchpoint[1]);
+          int tidx = tileIndex(touchPoint[0], touchPoint[1]);
           if (tidx != -1) {
             if ((tileStatus[tidx] & MASK_SELECTED_RD5) != 0) {
-              tileStatus[tidx] ^= MASK_SELECTED_RD5;
+              toggleTileStatus(tidx, MASK_SELECTED_RD5);
               if ((tileStatus[tidx] & MASK_INSTALLED_RD5) != 0) {
-                tileStatus[tidx] |= MASK_DELETED_RD5;
+                setTileStatus(tidx, MASK_DELETED_RD5);
               }
             } else if ((tileStatus[tidx] & MASK_DELETED_RD5) != 0) {
-              tileStatus[tidx] ^= MASK_DELETED_RD5;
+              toggleTileStatus(tidx, MASK_DELETED_RD5);
             } else {
-              tileStatus[tidx] ^= MASK_SELECTED_RD5;
+              toggleTileStatus(tidx, MASK_SELECTED_RD5);
             }
           }
         }
