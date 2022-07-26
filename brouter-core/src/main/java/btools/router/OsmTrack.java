30b6257776b4316867234ca40fea246b9f636328
Manuel Fuhr <manuel@knid.net>
2022-07-26 07:12:07 +0200

    Use me.qoomon.git-versioning to determine version

--- brouter-core/src/main/java/btools/router/OsmTrack.java
+++ brouter-core/src/main/java/btools/router/OsmTrack.java
@@ -36,8 +36,7 @@ import btools.util.FrozenLongMap;
 import btools.util.StringUtils;
 
 public final class OsmTrack {
-  final public static String version = "1.7.3";
-  final public static String versionDate = "19082023";
+  final private String version = getClass().getPackage().getImplementationVersion();
 
   // csv-header-line
   private static final String MESSAGES_HEADER = "Longitude\tLatitude\tElevation\tDistance\tCostPerKm\tElevCost\tTurnCost\tNodeCost\tInitialCost\tWayTags\tNodeTags\tTime\tEnergy";
