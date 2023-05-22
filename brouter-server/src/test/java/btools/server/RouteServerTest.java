d508337d7ea78fa7310966572fd5fa493e6fc0cd
Manuel Fuhr <manuel@knid.net>
2023-05-22 13:52:25 +0200

    Fix tests with elevation data

--- brouter-server/src/test/java/btools/server/RouteServerTest.java
+++ brouter-server/src/test/java/btools/server/RouteServerTest.java
@@ -91,7 +91,7 @@ public class RouteServerTest {
 
     InputStream inputStream = httpConnection.getInputStream();
     JSONObject geoJson = new JSONObject(new String(inputStream.readAllBytes(), StandardCharsets.UTF_8));
-    Assert.assertEquals("1866", geoJson.query("/features/0/properties/track-length"));
+    Assert.assertEquals("1570", geoJson.query("/features/0/properties/track-length"));
   }
 
   @Test
