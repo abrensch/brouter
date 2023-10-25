3c8fee7a0b354d2f69adaa664381e78431406e0f
Simon Densborn <simon.densborn@posteo.de>
2023-10-25 21:00:42 +0200

    Added 'DIV' expression for profiles

--- brouter-expressions/src/main/java/btools/expressions/BExpression.java
+++ brouter-expressions/src/main/java/btools/expressions/BExpression.java
@@ -9,14 +9,15 @@ final class BExpression {
 
   private static final int ADD_EXP = 20;
   private static final int MULTIPLY_EXP = 21;
-  private static final int MAX_EXP = 22;
-  private static final int EQUAL_EXP = 23;
-  private static final int GREATER_EXP = 24;
-  private static final int MIN_EXP = 25;
+  private static final int DIV_EXP = 22;
+  private static final int MAX_EXP = 23;
+  private static final int EQUAL_EXP = 24;
+  private static final int GREATER_EXP = 25;
+  private static final int MIN_EXP = 26;
 
-  private static final int SUB_EXP = 26;
-  private static final int LESSER_EXP = 27;
-  private static final int XOR_EXP = 28;
+  private static final int SUB_EXP = 27;
+  private static final int LESSER_EXP = 28;
+  private static final int XOR_EXP = 29;
 
   private static final int SWITCH_EXP = 30;
   private static final int ASSIGN_EXP = 31;
@@ -144,6 +145,8 @@ final class BExpression {
         exp.typ = AND_EXP;
       } else if ("multiply".equals(operator)) {
         exp.typ = MULTIPLY_EXP;
+      } else if ("div".equals(operator)) {
+        exp.typ = DIV_EXP;
       } else if ("add".equals(operator)) {
         exp.typ = ADD_EXP;
       } else if ("max".equals(operator)) {
@@ -277,6 +280,8 @@ final class BExpression {
         return op1.evaluate(ctx) - op2.evaluate(ctx);
       case MULTIPLY_EXP:
         return op1.evaluate(ctx) * op2.evaluate(ctx);
+      case DIV_EXP:
+        return op1.evaluate(ctx) / op2.evaluate(ctx);
       case MAX_EXP:
         return max(op1.evaluate(ctx), op2.evaluate(ctx));
       case MIN_EXP:
