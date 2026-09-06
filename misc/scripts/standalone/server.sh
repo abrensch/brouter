96888799e134d9a637ccf20c94af4f7522cab070
Quadrubo <71718414+Quadrubo@users.noreply.github.com>
2026-09-06 17:40:17 +0200

    feat(server): read JAVA_OPTS, PORT and MAXTHREADS from the environment

--- misc/scripts/standalone/server.sh
+++ misc/scripts/standalone/server.sh
@@ -5,7 +5,9 @@ cd "$(dirname "$0")"
 # java -cp brouter.jar btools.brouter.RouteServer <segmentdir> <profile-map> <customprofiledir> <port> <maxthreads> [bindaddress]
 
 # maxRunningTime is the request timeout in seconds, set to 0 to disable timeout
-JAVA_OPTS="-Xmx128M -Xms128M -Xmn8M -DmaxRunningTime=300 -DuseRFCMimeType=false"
+JAVA_OPTS=${JAVA_OPTS:-"-Xmx128M -Xms128M -Xmn8M -DmaxRunningTime=300 -DuseRFCMimeType=false"}
+PORT=${PORT:-17777}
+MAXTHREADS=${MAXTHREADS:-1}
 
 # If paths are unset, first search in locations matching the directory structure
 # as found in the official BRouter zip archive
@@ -28,4 +30,4 @@ if [ ! -e "$CUSTOMPROFILESPATH" ]; then
     CUSTOMPROFILESPATH="../customprofiles"
 fi
 
-java $JAVA_OPTS -cp $CLASSPATH btools.server.RouteServer "$SEGMENTSPATH" "$PROFILESPATH" "$CUSTOMPROFILESPATH" 17777 1 $BINDADDRESS
+exec java $JAVA_OPTS -cp $CLASSPATH btools.server.RouteServer "$SEGMENTSPATH" "$PROFILESPATH" "$CUSTOMPROFILESPATH" "$PORT" "$MAXTHREADS" $BINDADDRESS
